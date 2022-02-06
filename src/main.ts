2eed08af3b651bbdb4cd70512dacf1aab6e9a333
muyake <1245570818@qq.com>
2022-02-06 14:20:45 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import store from './store/index'
+import router from './router/index'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+createApp(App).use(ElementPlus).use(store).use(router).mount('#app')
